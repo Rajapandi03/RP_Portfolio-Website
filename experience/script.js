b622adf51ecb369a33428a1723e917be97227aab
Rajapandi03 <pandiraja981@gmail.com>
2025-06-14 21:18:38 +0530

    exp

--- experience/script.js
+++ experience/script.js
@@ -47,16 +47,23 @@ $(document).ready(function() {
 
 
     // Disable right-click and developer tools shortcuts
-// Disable right-click and developer tools shortcuts
-document.onkeydown = function(e) {
-    if (e.keyCode == 123) { // F12
-        return false;
+    document.onkeydown = function(e) {
+        if (e.keyCode == 123) { // F12
+            return false;
+        }
+        if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) { // Ctrl+Shift+I
+            return false;
+        }
+        if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) { // Ctrl+Shift+C
+            return false;
+        }
+        if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) { // Ctrl+Shift+J
+            return false;
+        }
+        if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) { // Ctrl+U
+            return false;
+        }
     }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) { // Ctrl+Shift+I
-        return false;
-    }
-    // ... and so on
-}
 
     // Change page title and favicon when tab is not active
     document.addEventListener('visibilitychange', function() {
